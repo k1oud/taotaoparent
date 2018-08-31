9d79110037a3e6b70dd0114a9475a44aafd12595
wangganyi <kloud0919@163.com>
2018-08-31 17:14:15 +0800

    修复

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/TbItemService.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/TbItemService.java
@@ -1,11 +1,9 @@
 package com.taotao.service;
 
 
-import com.taotao.mapper.TbItemMapper;
+
 
 import com.taotao.pojo.TbItem;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 
 public interface TbItemService {
@@ -16,6 +14,9 @@ public interface TbItemService {
    TbItemMapper TB_ITEM_MAPPER;*/
 
     TbItem selectByPrimaryKey(long id);
+
+
+
 
 
 
